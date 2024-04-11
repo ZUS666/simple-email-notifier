207bb9373360039d93e60c30504ffface4ccb94a
zus666 <t4g1.86@gmail.com>
2024-04-11 10:15:26 +0500

    add template

--- email_notify_service/domain/constants.py
+++ email_notify_service/domain/constants.py
@@ -3,7 +3,7 @@ import enum
 
 class EmailSubject(enum.StrEnum):
     activation_code = 'Activation code'
-    reset_password = 'Reset password'
+    reset_password = 'Reset password code'
 
 
 class DefaultName(enum.StrEnum):
